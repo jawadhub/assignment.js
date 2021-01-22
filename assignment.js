b6fd7a7ec24ba60c231ea6e0f516ac383d712da6
jawadhub <jawad.dp@gmail.com>
2021-01-22 20:14:43 +0600

    update

--- assignment.js
+++ assignment.js
@@ -2,8 +2,14 @@
 
 // Working on Problem-1 : kilometerToMeter Function
 function kilometerToMeter(kilometer){
-  var meter = kilometer * 1000;
-  return meter;
+  if(kilometer < 0){
+    return "You can't entered negative distance."
+  }
+
+  else{
+    var meter = kilometer * 1000;
+    return meter;
+  }
 }
 
 
@@ -11,37 +17,47 @@ function kilometerToMeter(kilometer){
 
 // Working on Problem-2 : budgetCalculator Function
 function budgetCalculator(watch, mobile, laptop,){
-  itemWatch = watch * 50;
-  itemMobile = mobile * 100;
-  itemLaptop = laptop * 500;
-  total = itemWatch + itemMobile + itemLaptop;
-  return total;
+if(watch <0 || mobile < 0 || laptop <0){
+  return "You can't entered negative amount."
+  }
+
+else{
+    itemWatch = watch * 50;
+    itemMobile = mobile * 100;
+    itemLaptop = laptop * 500;
+    total = itemWatch + itemMobile + itemLaptop;
+    return total;
+  }
 }
 
 
 
 
-// Working on Problem-3 : hotelCost Function
 function hotelCost(perDay){
   var stayDays = 0;
 
-  if(perDay <= 10){
-    stayDays = perDay * 100;
-  }
-  else if(perDay <= 20){
-    var firstPart = 10 * 100;
-    var remaining = perDay - 10;
-    var secondPart = remaining * 80;
-    stayDays = firstPart + secondPart;
-  }
-  else{
-    var firstPart = 10 * 100;
-    var secondPart = 10 * 80;
-    var remaining = perDay - 20;
-    var thirdpart = remaining * 50;
-    stayDays = firstPart + secondPart + thirdpart;
-  }
-  return stayDays;
+    if(perDay < 0 ){
+      return "You can't entered negative days"
+    }
+
+    else if(perDay <= 10){
+      stayDays = perDay * 100;
+    }
+
+    else if(perDay <= 20){
+      var firstPart = 10 * 100;
+      var remaining = perDay - 10;
+      var secondPart = remaining * 80;
+      stayDays = firstPart + secondPart;
+    }
+    else{
+      var firstPart = 10 * 100;
+      var secondPart = 10 * 80;
+      var remaining = perDay - 20;
+      var thirdpart = remaining * 50;
+      stayDays = firstPart + secondPart + thirdpart;
+    }
+    return stayDays;
 }
 
 
